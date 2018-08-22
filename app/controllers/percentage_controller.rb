da19d384b2eb0ad2fb64d37f72d95bd2e200fe4f
Manh Cuong <manhcuongueh@gmail.com>
2018-08-22 11:07:34 +1000

    add cm and reply

--- app/controllers/percentage_controller.rb
+++ app/controllers/percentage_controller.rb
@@ -3,5 +3,7 @@ class PercentageController < ApplicationController
         id = params[:id]
         @user=User.find_by_id(id)
         @percentages = @user.percentages
+        @respond_times = @percentages.inject(0){|sum,x| sum + x.reply_time }
+        @all_comments = @percentages.inject(0){|sum,x| sum + x.total_cm }
     end
 end
